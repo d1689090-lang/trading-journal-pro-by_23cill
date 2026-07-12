01ade1031ecf093603ba0fd031431e4bab5c0ad5
d1689090-lang <d1689090@gmail.com>
2026-07-13 00:55:22 +0700

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -1,1 +1,43 @@
+const trades = JSON.parse(localStorage.getItem("trades")) || [];
 
+const events = trades.map(trade => {
+
+    let color = "#facc15";
+
+    if(trade.result=="WIN") color="#22c55e";
+
+    if(trade.result=="LOSE") color="#ef4444";
+
+    return{
+
+        title:`${trade.pair} ${trade.profit}$`,
+
+        start:trade.date,
+
+        color:color
+
+    }
+
+});
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+const calendar = new FullCalendar.Calendar(
+
+document.getElementById("calendar"),
+
+{
+
+initialView:"dayGridMonth",
+
+height:"auto",
+
+events:events
+
+}
+
+);
+
+calendar.render();
+
+});
